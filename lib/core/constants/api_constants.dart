adf505ddd07969d412935d0988c74541235b8560
muhamadayeshaaulia <shaxyyy.03@gmail.com>
2026-06-09 09:34:29 +0700

    feat : merubah api constan
    merubah api constan karena berbeda lokasi lagi hehe

--- lib/core/constants/api_constants.dart
+++ lib/core/constants/api_constants.dart
@@ -1,7 +1,5 @@
-
-
 class ApiConstants {
-  static const String baseUrl = 'http://192.168.8.244:8080/v1';
+  static const String baseUrl = 'http://172.20.10.4:8080/v1';
 
   // Wallet endpoints
   static const String wallet = '/wallet';
