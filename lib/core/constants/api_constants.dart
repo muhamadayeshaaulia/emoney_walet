017dadcd04204b0befda7ef122d5028bcb783c7d
muhamadayeshaaulia <shaxyyy.03@gmail.com>
2026-06-12 23:21:18 +0700

    feat : merubah api constants
    merubah api karena merubah backena memisahkan antara backend emony dan ecomerce

--- lib/core/constants/api_constants.dart
+++ lib/core/constants/api_constants.dart
@@ -1,9 +1,13 @@
 class ApiConstants {
   static const String baseUrl = 'http://192.168.100.123:8080/v1';
-  // Wallet endpoints
-  static const String wallet = '/wallet';
-  static const String topUp = '/wallet/topup';
-  static const String pay = '/wallet/pay';
+
+  // Account endpoints
+  static const String wallet = '/account'; // Dulu /wallet
+  static const String transactions = '/account/transactions';
+
+  // Payment endpoints
+  static const String topUp = '/payment/topup';
+  static const String pay = '/payment/transfer'; // Pindah dari /wallet/pay
 
   // Auth endpoints
   static const String verifyToken = '/auth/verify-token';
